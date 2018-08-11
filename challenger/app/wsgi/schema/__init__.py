5376a20cc8a2a0ff814251b473fa409e2c6051aa
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 22:42:54 +0200

    qsa: Compile top-level HTTP endpoint schema package

--- challenger/app/wsgi/schema/__init__.py
+++ challenger/app/wsgi/schema/__init__.py
@@ -1,4 +1,5 @@
 from .challenge import Challenge
+from .duplicateentity import DuplicateEntity
 from .error import Error
 from .health import Health
 from .servicenotavailable import ServiceNotAvailable
