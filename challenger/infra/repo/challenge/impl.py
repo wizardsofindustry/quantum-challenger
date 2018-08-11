93ca8536cbd0a10b0f83cfa70a5aae0ec74d7a71
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 21:52:22 +0200

    Implement ChallengeRepository.exists()

--- challenger/infra/repo/challenge/impl.py
+++ challenger/infra/repo/challenge/impl.py
@@ -1,13 +1,30 @@
+"""Declares the :class:`ChallengeRepository`."""
+import sqlalchemy
+
+from ...orm import PhonenumberChallenge
 from .base import BaseChallengeRepository
 
 
 class ChallengeRepository(BaseChallengeRepository):
+    """Provides an interface to manipulate the persisted state of the
+    :mod:`challenger` application.
+    """
+    models = {
+        'sms': PhonenumberChallenge,
+        'voice': PhonenumberChallenge
+    }
 
     def persist(self, dto):
         raise NotImplementedError("Subclasses must override this method.")
 
     def exists(self, using, recipient):
-        raise NotImplementedError("Subclasses must override this method.")
+        """Return a boolean indicating if a challenge over the delivery
+        mechanism `using` exists for the specified `recipient`.
+        """
+        Relation = self.models[using]
+        query = sqlalchemy.exists()\
+            .where(Relation.recipient == recipient)
+        return self.session.query(query).scalar()
 
     def delete(self, using, recipient):
         raise NotImplementedError("Subclasses must override this method.")
