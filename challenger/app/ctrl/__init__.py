a225ff62f0f0b9a4dba06d6a6dbe48c1be900344
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 06:30:00 +0000

    qsa: Compile top-level controllers package.

---
+++ challenger/app/ctrl/__init__.py
@@ -0,0 +1,4 @@
+"""Contains all controller implementations that are used by the WSGI
+appication. See also :class:`~challenger.app.wsgi.application.WSGIApplication`.
+"""
+from .challenge import ChallengeCtrl
