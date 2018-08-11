b3e2e799192efa1b87ff68c86c40e0e9c9e6078a
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 22:31:49 +0200

    qsa: Compile endpoints from OpenAPI definition

--- challenger/app/wsgi/endpoints/__init__.py
+++ challenger/app/wsgi/endpoints/__init__.py
@@ -2,12 +2,14 @@ from werkzeug.routing import Map
 
 from .health import HealthEndpoint
 from .version import VersionEndpoint
+from .verify import VerifyEndpoint
 from .challenge import ChallengeEndpoint
 
 
 urlpatterns = Map([
     HealthEndpoint.as_rule(),
     VersionEndpoint.as_rule(),
+    VerifyEndpoint.as_rule(),
     ChallengeEndpoint.as_rule(),
 ])
 
