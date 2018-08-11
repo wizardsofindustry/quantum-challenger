72911c350e485673943cb25dfe2a9cfe2a17fb01
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 06:30:00 +0200

    Create stub implementations for PUT,POST

--- challenger/app/ctrl/challenge/impl.py
+++ challenger/app/ctrl/challenge/impl.py
@@ -3,9 +3,18 @@ from .base import BaseChallengeCtrl
 
 
 class ChallengeCtrl(BaseChallengeCtrl):
+    """Exposes handler functions for ``POST`` and ``PUT`` requests
+    to the ``/challenge`` endpoint.
+    """
 
     async def post(self, request, *args, **kwargs):
-        raise NotImplementedError("Subclasses must override this method.")
+        """Create a new challenge to the recipient specified in the
+        request entity.
+        """
+        return self.render_to_response(ctx={}, status_code=202)
 
     async def put(self, request, *args, **kwargs):
-        raise NotImplementedError("Subclasses must override this method.")
+        """Re-issue a challenge to the recipient specified in the
+        request entity.
+        """
+        return self.render_to_response(ctx={}, status_code=202)
