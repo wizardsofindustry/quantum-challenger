e6e2d5564c4913fa6c24e9c6f13d3a143eec5c00
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-11 21:18:28 +0200

    qsa: Compile application runtime

--- challenger/runtime/__main__.py
+++ challenger/runtime/__main__.py
@@ -30,6 +30,7 @@ if os.getenv('CHALLENGER_SECRETS'):
 os.environ['SQ_ENVIRON_PREFIX'] = 'CHALLENGER'
 DEFAULT_SECRET_KEY = "b9d3fc670302b2191fd341814ee98a13ec2070657f816cf2f93d2dc282277657"
 os.environ.setdefault('CHALLENGER_SECRET_KEY', "b9d3fc670302b2191fd341814ee98a13ec2070657f816cf2f93d2dc282277657")
+os.environ.setdefault('CHALLENGER_RDBMS_DSN', "postgresql+psycopg2://challenger:challenger@rdbms:5432/challenger")
 os.environ.setdefault('CHALLENGER_HTTP_ADDR', "0.0.0.0")
 os.environ.setdefault('CHALLENGER_HTTP_PORT', "8443")
 
