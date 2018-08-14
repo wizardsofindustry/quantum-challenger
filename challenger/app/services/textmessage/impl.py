f9162cfa886af72128971947dbedeb981ecef1de
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-14 20:08:26 +0200

    Actually send the message

--- challenger/app/services/textmessage/impl.py
+++ challenger/app/services/textmessage/impl.py
@@ -1,4 +1,5 @@
 """Declares :class:`TextMessageService`."""
+import challenger.environ
 from .base import BaseTextMessageService
 
 
@@ -6,7 +7,18 @@ class TextMessageService(BaseTextMessageService):
     """Exposes an interface to send Short Message Service (SMS)
     messages.
     """
+    base_url = "https://rest.messagebird.com"
 
     def send(self, sender, recipient, message):
         """Send a SMS message to the specified `recipient`."""
-        pass
+        headers = {'Accept': "application/json",
+            'Authorization': f'AccessKey {challenger.environ.MESSAGEBIRD_ACCESS_KEY}'}
+        response = self.http.post(f'{self.base_url}/messages',
+            json={
+                'originator': sender,
+                'recipients': [recipient] if isinstance(recipient, str)\
+                    else recipient,
+                'body': message
+            },
+            headers=headers)
+        response.raise_for_status()
